4439d606bc9d53bd135d61b2a9b9ef66d9322e14
Tuan <PATuan262004@gmail.com>
2025-10-13 15:03:48 +0700

    config styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-
 function App() {
-  return (
-    <>
-      <div>
-        ha lo
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div>ha lo</div>
+        </>
+    );
 }
 
-export default App
+export default App;
